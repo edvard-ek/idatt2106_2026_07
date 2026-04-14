76f31833d71b4589f61a0371a8b0ee7e1006e951
Edvard Klavenes <edvardklavenes@gmail.com>
2026-04-14 11:05:47 +0200

    chore: set up backend

--- backend/src/main/java/backend/idatt2106_2026_07/auth/api/dto/LoginRequest.java
+++
@@ -1,4 +0,0 @@
-package backend.idatt2106_2026_07.auth.api.dto;
-
-public record LoginRequest(String email, String password) {
-}
--- backend/src/main/java/backend/idatt2106_2026_07/auth/api/dto/LoginResponse.java
+++
@@ -1,4 +0,0 @@
-package backend.idatt2106_2026_07.auth.api.dto;
-
-public record LoginResponse(String token, String role) {
-}

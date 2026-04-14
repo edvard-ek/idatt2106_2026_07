4826be8cb79bc908b38e17ec8a0959c20049562e
Liam Lande <liam.lande@gmail.com>
2026-04-14 16:17:05 +0200

    feat: configure application properties and add initial data for schools, classrooms, users, and teachers

---
+++ backend/game/src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO schools (id, name, email_suffix)
+VALUES (1, 'Uranienborg Skole', '@osloskolen.no');
+
+INSERT INTO classrooms (id, name, head_teacher_id, school_id)
+VALUES (1, '8A', NULL, 1);
+
+INSERT INTO users (id, username, email, first_name, last_name, password)
+VALUES (1, 'alf', 'alf@osloskolen.no', 'Test', 'Teacher', '$2y$10$exUh84ts9h5pXIm.QSqA3uHQ9M5ifNW.ePQs7lBNYDeCV7RZoY5ja');--Password123!
+
+INSERT INTO teachers (id, school_id)
+VALUES (1, 1);
+
+UPDATE classrooms
+SET head_teacher_id = 1
+WHERE id = 1;
+
+INSERT INTO classroom_teachers (classroom_id, teacher_id)
+VALUES (1, 1);
+
+INSERT INTO users (id, username, email, first_name, last_name, password)
+VALUES (2, 'snorre', 'snorre@osloskolen.no', 'Test', 'Pupil', '$2y$10$exUh84ts9h5pXIm.QSqA3uHQ9M5ifNW.ePQs7lBNYDeCV7RZoY5ja'); --Password123!
+
+INSERT INTO pupils (id, classroom_id)
+VALUES (2, 1);
+
+ALTER TABLE schools ALTER COLUMN id RESTART WITH 2;
+ALTER TABLE classrooms ALTER COLUMN id RESTART WITH 2;
+ALTER TABLE users ALTER COLUMN id RESTART WITH 3;
